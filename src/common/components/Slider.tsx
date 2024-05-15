1d83d69def2ba1241cb0697edf97c60d7b629a5d
Donny Verduijn <donnyverduijn@gmail.com>
2024-05-15 19:58:12 +0000

    remove slider cruft

--- src/common/components/Slider.tsx
+++ src/common/components/Slider.tsx
@@ -34,7 +34,6 @@ interface Props<T> extends DefaultProps {
 
 export const SliderBase = <T extends number>(props: Props<T>) => {
   const { className, min, max, track, getValue, onChange, ...rest } = props;
-  const slots = React.useRef({ thumb: ThumbComponent });
 
   const handleChange = React.useCallback<
     (e: Event, v: number | number[]) => void
@@ -46,7 +45,6 @@ export const SliderBase = <T extends number>(props: Props<T>) => {
       max={max ?? 255}
       min={min ?? 0}
       onChange={handleChange}
-      slots={slots.current}
       track={track ?? 'normal'}
       value={getValue()}
       valueLabelDisplay='off'
@@ -69,32 +67,5 @@ export const Slider = styled(observer(SliderBase))`
 
   & .MuiSlider-thumb {
     background-color: #fff;
-    border: 2px solid currentcolor;
-
-    & .bar {
-      background-color: currentcolor;
-      height: 8px;
-      margin-left: 1px;
-      margin-right: 1px;
-      width: 1px;
-    }
   }
 `;
-
-interface ThumbComponentProps extends React.HTMLAttributes<EventTarget> {}
-
-// eslint-disable-next-line react/no-multi-comp
-const ThumbComponent: React.FC<ThumbComponentProps> = ({
-  children,
-  ...other
-}) => {
-  return (
-    // eslint-disable-next-line react/jsx-props-no-spreading
-    <SliderThumb {...other}>
-      {children}
-      <span className='bar' />
-      <span className='bar' />
-      <span className='bar' />
-    </SliderThumb>
-  );
-};
