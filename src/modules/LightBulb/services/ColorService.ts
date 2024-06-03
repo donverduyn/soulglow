707843b060926279bb1b44ae049fbe2255715fd4
Donny Verduijn <donnyverduijn@gmail.com>
2024-06-03 14:30:43 +0000

    feat: add ColorService definition to support complex api interactions

---
+++ src/modules/LightBulb/services/ColorService.ts
@@ -0,0 +1,19 @@
+import { Effect, pipe } from 'effect';
+
+// this service should be responsible for updating the colors of lightbulbs,
+// It delegates with one or multiple calls to the injected repositories
+// Ultimately it supports
+export const ColorService = () => {
+  const updateColor = (color: {
+    hue: number;
+    level: number;
+    saturation: number;
+  }) => {
+    // this is a placeholder for the actual implementation
+    return pipe(Effect.sync(() => color));
+  };
+
+  return {
+    updateColor,
+  };
+};
