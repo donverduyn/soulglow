8f800240d0c989d82d4ce0db08a47209dbb3c954
DonnyVerduijn <donnyverduijn@gmail.com>
2024-06-27 08:22:48 +0000

    WIP

--- src/modules/EndpointPanel/context.ts
+++ src/modules/EndpointPanel/context.ts
@@ -48,32 +48,24 @@ const createProviderContext = <R, S extends Context.TagClass<any, any, any>[]>(
   return React.createContext(map);
 };
 
-type InferTagId<T> =
-  T extends Context.TagClass<any, infer I extends AnyKey, any> ? I : never;
-
 type AnyKey = string | number | symbol;
-type IsAnyKey<T> = T extends AnyKey ? T : never;
-type InferKey<T> = T extends AnyKey
-  ? T
-  : InferTagId<T> extends AnyKey
-    ? InferTagId<T>
-    : never;
+type KeyType<T, T1> =
+  | (T extends AnyKey ? T : never)
+  | Context.TagClass<any, T1, any>;
 
 // we infer separately to keep the literal type of the tag key
 // instead of falling back to AnyKey, because it is the common denominator between the two
 interface TypedMap<T> {
-  get<K extends keyof T, K1 extends keyof T>(
-    key: IsAnyKey<K> | Context.TagClass<any, K1, any>
-  ): T[K & K1];
+  get<K extends keyof T, K1 extends keyof T>(key: KeyType<K, K1>): T[K & K1];
   set<K extends keyof T, K1 extends keyof T>(
-    k: IsAnyKey<K> | Context.TagClass<any, K1, any>,
+    k: KeyType<K, K1>,
     v: T[K & K1]
   ): void;
 }
 
 export const withKey =
   <K extends keyof T, K1 extends keyof T, V, T extends Record<string, unknown>>(
-    key: IsAnyKey<K> | Context.TagClass<any, K1, any>,
+    key: KeyType<K, K1>,
     value: V
   ) =>
   (createMap: () => TypedMap<T>) => {
@@ -81,7 +73,7 @@ export const withKey =
       const map = createMap();
       const resultKey = Context.isTag(key) ? key.key : key;
       map.set(resultKey as keyof T, value as T[keyof T]);
-      return map as TypedMap<T & Record<InferKey<K & K1>, V>>;
+      return map as TypedMap<T & Record<K & K1, V>>;
     };
   };
 
@@ -114,10 +106,10 @@ myMap.set('name', 'Typed Map');
 myMap.set('isActive', true);
 myMap.set(EndpointStore, 'foo');
 
-const id = myMap.get(''); // 101
-const name = myMap.get('name'); // 'Typed Map'
-const isActive = myMap.get('isActive'); // true
-const store = myMap.get(EndpointStore); // 'foo'
-const repo = myMap.get(DeviceRepo); // 0
+const id = myMap.get('id'); // number
+const name = myMap.get('name'); // string
+const isActive = myMap.get('isActive'); // boolean
+const store = myMap.get(EndpointStore); // string
+const repo = myMap.get(DeviceRepo); // number
 
 const ProviderContext = createProviderContext([DeviceRepo]);
