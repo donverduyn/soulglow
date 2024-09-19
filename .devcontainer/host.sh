5819e76109c5278ec9e5b4c212666b1ca207cc3a
Donny Verduijn <donnyverduijn@gmail.com>
2024-09-19 10:12:13 +0000

    WIP

--- .devcontainer/host.sh
+++ .devcontainer/host.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-
 # make sure the config directory exists to bind mount
-mkdir -p "$HOME/.minikube"
-mkdir -p "$HOME/.kube"
+# mkdir -p "$HOME/.minikube"
+# mkdir -p "$HOME/.kube"
 
-# Check if the /tmp/monaspace directory does not exist
+# Install monaspace
+
 if [ ! -d "/tmp/monaspace" ]; then
-    # Clone the repository into /tmp/monaspace
     git clone https://github.com/githubnext/monaspace.git /tmp/monaspace > /dev/null 2>&1
-
-    # Change directory to /tmp/monaspace
     cd /tmp/monaspace
 
-    # Make the installation script executable
-    chmod +x ./util/install_linux.sh
-
-    # Run the installation script with all output silenced
-    ./util/install_linux.sh > /dev/null 2>&1
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        chmod +x ./util/install_linux.sh
+         ./util/install_linux.sh > /dev/null 2>&1
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        chmod +x ./util/install_macos.sh
+         ./util/install_macos.sh > /dev/null 2>&1
+    fi
 fi
