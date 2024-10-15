23098e42e235d9666ac2558c183cc74f50aa7eed
Donny Verduijn <donnyverduijn@gmail.com>
2024-10-15 21:51:55 +0000

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -91,8 +91,8 @@ export default defineConfig(({ mode }) => ({
         lintCommand:
           mode === 'test'
             ? // exclude tsx files for eslint during test for now
-              "eslint 'tests/**/*.ts' 'src/**/*.test.ts' --cache --cache-location node_modules/.cache/.eslintcache"
-            : 'eslint ./**/*.{js,cjs,ts,tsx} --cache --cache-location node_modules/.cache/.eslintcache',
+              "eslint 'tests/**/*.ts' 'src/**/*.test.ts' --cache --cache-location ./.cache/.eslintcache"
+            : 'eslint ./**/*.{js,cjs,ts,tsx} --cache --cache-location ./.cache/eslint/.eslintcache',
         useFlatConfig: false,
       },
       overlay: {
