d53159b0e9e053a3e1258c6eccb08769a929867a
Donny Verduijn <donnyverduijn@gmail.com>
2024-10-15 21:43:56 +0000

    WIP

--- src/modules/EndpointPanel/main.stories.ts
+++ src/modules/EndpointPanel/main.stories.ts
@@ -1,7 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import type { Simplify } from 'type-fest';
 import { WithRuntime } from 'common/components/hoc/withRuntime';
 import { AppRuntime } from 'modules/App/context';
+import * as AppTags from 'modules/App/tags';
 import EndpointPanel from './main';
+
+type Foo = Simplify<typeof AppTags>;
 
 // TODO: import tags to type test implementation against them
 // TODO: we need to think about how we want to spy on effectful deps
